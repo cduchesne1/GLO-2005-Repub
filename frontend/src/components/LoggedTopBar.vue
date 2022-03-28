ecf71790629690205df795a1662600b335ae016c
Christophe Duchesne <cduchesne.1@gmail.com>
2022-03-27 23:05:00 -0400

    feat: logged bar and profile infos

---
+++ frontend/src/components/LoggedTopBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-evenly items-center px-24 py-10">
+    <div class="flex items-center">
+      <img src="../assets/rePubIcon.svg" />
+      <input
+        type="text"
+        v-model="search"
+        placeholder="Search"
+        class="bg-gray-900 text-white w-96 py-2 px-2 rounded-lg mx-4"
+      />
+      <a class="text-base text-gray-500 mr-4">Tasks</a>
+      <a class="text-base text-gray-500">Explore</a>
+    </div>
+    <img src="../assets/profileIcon.svg" />
+  </div>
+</template>
--- frontend/src/navigation/router/index.js
+++ frontend/src/navigation/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import HomePage from "@/pages/HomePage";
+import ProfilePage from "@/pages/ProfilePage";
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: "/",
       name: "Home",
       component: HomePage,
+    },
+    {
+        path: "/profile",
+        name: "Profile",
+        component: ProfilePage,
     },
   ],
 });
