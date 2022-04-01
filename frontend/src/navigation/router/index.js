0b00652d1fd6ef32c3cc54569ee3db362a5afd64
Christophe Duchesne <cduchesne.1@gmail.com>
2022-03-31 22:11:15 -0400

    feat: sign up page static UI

--- frontend/src/navigation/router/index.js
+++ frontend/src/navigation/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import HomePage from "@/pages/HomePage";
 import LoginPage from "@/pages/LoginPage";
+import SignUp from "@/pages/SignUp";
 import ProfilePage from "@/pages/ProfilePage";
 
 Vue.use(Router);
@@ -17,6 +18,11 @@ export default new Router({
       path: "/login",
       name: "Login",
       component: LoginPage,
+    },
+    {
+      path: "/signup",
+      name: "Signup",
+      component: SignUp,
     },
     {
         path: "/profile",
