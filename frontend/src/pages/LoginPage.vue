a6f898f82dd3d41aaa88914eb30b6785d646121c
Christophe Duchesne <cduchesne.1@gmail.com>
2022-03-31 21:56:05 -0400

    feat: login static UI

---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-gray-800 w-full h-full flex flex-col min-h-screen items-center justify-center">
+        <img class="w-24" src="../assets/rePubIcon.svg" />
+        <h1 class="text-3xl text-white font-bold max-w-xl mt-8">Sign in to RePub</h1>
+        <div class="flex flex-col bg-white bg-opacity-10 border-2 border-gray-300 rounded-xl p-8 mt-8">
+            <label class="text-md text-white mb-2">Username or email address</label>
+            <input type="text" v-model="username" class="bg-gray-800 text-white w-96 py-2 px-2 rounded-lg"/>
+            <label class="text-md text-white mt-8 mb-2">Password</label>
+            <input type="password" v-model="username" class="bg-gray-800 text-white w-96 py-2 px-2 rounded-lg"/>
+            <button
+            class="bg-pink-600 hover:bg-pink-700 text-white text-sm font-bold py-2 px-8 rounded mt-8"
+          >
+            Sign In
+          </button>
+        </div>
+        <div class="flex bg-white bg-opacity-10 border-2 border-gray-300 rounded-xl p-4 mt-8">
+            <a class="text-md text-white mr-2">Don't have an account?</a>
+            <a class="text-md text-pink-600">Sign Up</a>
+        </div>
+    </div>
+</template>
