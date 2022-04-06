87a0e462c240ea42fd47555835790535c563d15a
Christophe Duchesne <cduchesne.1@gmail.com>
2022-04-05 22:48:30 -0400

    fix: change signup ui

--- frontend/src/pages/SignUp.vue
+++ frontend/src/pages/SignUp.vue
@@ -5,10 +5,10 @@
         <div class="flex flex-col bg-white bg-opacity-10 border-2 border-gray-300 rounded-xl p-8 mt-8">
             <label class="text-md text-white mb-2">Email</label>
             <input type="email" v-model="email" class="bg-gray-800 text-white w-96 py-2 px-2 rounded-lg"/>
-            <label class="text-md text-white mt-8 mb-2">Full Name</label>
-            <input type="text" v-model="name" class="bg-gray-800 text-white w-96 py-2 px-2 rounded-lg"/>
             <label class="text-md text-white mt-8 mb-2">Username</label>
             <input type="text" v-model="username" class="bg-gray-800 text-white w-96 py-2 px-2 rounded-lg"/>
+            <label class="text-md text-white mt-8 mb-2">Full Name</label>
+            <input type="text" v-model="name" class="bg-gray-800 text-white w-96 py-2 px-2 rounded-lg"/>
             <label class="text-md text-white mt-8 mb-2">Password</label>
             <input type="password" v-model="password" class="bg-gray-800 text-white w-96 py-2 px-2 rounded-lg"/>
             <button
