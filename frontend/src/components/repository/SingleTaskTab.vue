be8df0beddd5efb4d54533faf9c2860ff482a824
Christophe Duchesne <cduchesne.1@gmail.com>
2022-04-16 00:21:03 -0400

    feat: connect single task page

--- frontend/src/components/repository/SingleTaskTab.vue
+++ frontend/src/components/repository/SingleTaskTab.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col mt-16">
-    <div class="flex flex-col border-b border-solid border-gray-500 pb-4">
+    <div v-if="task" class="flex flex-col border-b border-solid border-gray-500 pb-4">
       <div class="flex justify-between w-full">
         <div class="flex items-center">
           <h2 class="text-4xl text-white mr-4">
-            This is the title of the task
+            {{ task.title }}
           </h2>
-          <h2 class="text-4xl text-gray-500">#32</h2>
+          <h2 class="text-4xl text-gray-500">{{ `#${task.number}` }}</h2>
         </div>
         <div class="flex items-center">
           <button
@@ -25,51 +25,59 @@
       <div class="flex items-center">
         <div
           class="bg-green-500 rounded-full py-2 px-4 text-white w-16 flex justify-center mr-4"
+          :class="task.state == 'closed' ? 'bg-purple-500' : ''"
         >
-          Open
+          {{ task.state.charAt(0).toUpperCase() + task.state.slice(1) }}
         </div>
-        <a class="text-gray-500 text-base font-bold mr-2">johndoe1</a>
-        <a class="text-gray-500 text-base"
-          >opened this issue 15 days ago - 0 comments</a
-        >
+        <a class="text-gray-500 text-base font-bold mr-2">{{
+          task.creator.username
+        }}</a>
+        <a class="text-gray-500 text-base">{{
+          `opened this task ${timeAgo(task.timestamp)} - 0 comments`
+        }}</a>
       </div>
     </div>
     <div class="flex">
       <div class="flex flex-col flex-1">
-        <div class="border border-solid border-gray-500 rounded-lg w-full mt-4">
+        <div v-if="task" class="border border-solid border-gray-500 rounded-lg w-full mt-4">
           <div class="flex bg-pink-500 bg-opacity-25 px-8 py-4 justify-between">
             <div>
-              <a class="text-base text-white font-bold mr-4">johndoe1</a>
-              <a class="text-base text-gray-500">commented 10 days ago</a>
+              <a class="text-base text-white font-bold mr-4">{{
+                task.creator.username
+              }}</a>
+              <a class="text-base text-gray-500">{{
+                `commented ${timeAgo(task.timestamp)}`
+              }}</a>
             </div>
             <a class="text-base text-gray-500 hover:text-pink-500">Edit</a>
           </div>
-          <div class="text-white max-w-5xl px-4 py-4">
-            This is the content of the comment. There is some amazing stuff in
-            here yolo.
+          <div class="text-white px-4 py-4">
+            {{ task.description }}
           </div>
         </div>
         <div class="flex flex-col mt-16">
           <div
+            v-for="comment in comments"
+            :key="comment.id"
             class="border border-solid border-gray-500 rounded-lg w-full mt-4"
           >
             <div
               class="flex bg-gray-500 bg-opacity-25 px-8 py-4 justify-between"
             >
               <div>
-                <a class="text-base text-white font-bold mr-4">johndoe1</a>
-                <a class="text-base text-gray-500">commented 10 days ago</a>
+                <a class="text-base text-white font-bold mr-4">{{ comment.sender.username }}</a>
+                <a class="text-base text-gray-500">{{ `commented ${timeAgo(comment.timestamp)}` }}</a>
               </div>
               <a class="text-base text-gray-500 hover:text-pink-500">Edit</a>
             </div>
-            <div class="text-white max-w-5xl px-4 py-4">
-              This is the content of the comment. There is some amazing stuff in
-              here yolo.
+            <div class="text-white px-4 py-4">
+              {{ comment.comment }}
             </div>
           </div>
         </div>
       </div>
       <div
+        v-if="task"
         class="flex flex-col ml-16 mt-4 border-b border-solid border-gray-500 h-24"
       >
         <div class="flex justify-between w-56">
@@ -78,16 +86,40 @@
             >Add</a
           >
         </div>
-        <a class="text-base text-gray-500 mt-4">No one - assign yourself</a>
+        <a v-if="!task.assigned" class="text-base text-gray-500 mt-4">No one - assign yourself</a>
+        <a v-if="task.assigned" class="text-base text-gray-500 mt-4">{{ task.assigned }}</a>
       </div>
     </div>
   </div>
 </template>
 <script>
+import moment from "moment";
+import {
+  fetchTaskByUsernameRepositoryAndNumber,
+  fetchTaskComments,
+} from "@/api/taskApi";
+
 export default {
+  data() {
+    return {
+      task: null,
+      comments: [],
+    };
+  },
+  async created() {
+    this.task = await fetchTaskByUsernameRepositoryAndNumber(
+      this.$route.params.username,
+      this.$route.params.repository,
+      this.$route.params.number
+    );
+    this.comments = await fetchTaskComments(this.task.id);
+  },
   methods: {
     createNewTask() {
-      this.$router.push("/repository/tasks/new");
+      this.$router.push(`/${this.$route.params.username}/${this.$route.params.repository}/tasks/new`);
+    },
+    timeAgo(timestamp) {
+      return moment(Date.parse(timestamp)).fromNow();
     },
   },
 };
