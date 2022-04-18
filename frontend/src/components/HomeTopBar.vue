51fd1c11fc6183a56b02ee089a8e0a6d3352df23
alexaubut <74124085+alexaubut@users.noreply.github.com>
2022-04-18 18:15:59 -0400

    plugged homePage (#110)
    Co-authored-by: Alexandre <alexandre.aubut.1@ulaval.ca>

--- frontend/src/components/HomeTopBar.vue
+++ frontend/src/components/HomeTopBar.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex justify-evenly items-center px-24 py-10">
     <div class="flex">
-      <img src="../assets/rePubIcon.svg" />
+      <img
+        @click="goToHome()"
+        src="../assets/rePubIcon.svg"
+        class="cursor-pointer"
+      />
       <h2 class="text-white font-bold text-lg ml-4">RePub</h2>
     </div>
     <input
@@ -14,14 +18,36 @@
       <a class="text-base text-gray-500">Explore</a>
       <button
         class="bg-pink-600 hover:bg-pink-700 text-white text-sm font-bold py-2 px-8 rounded mx-4"
+        @click="goToSignUp"
       >
         Sign Up
       </button>
       <button
         class="border-pink-600 hover:border-pink-700 hover:text-pink-700 text-pink-600 text-sm font-bold py-2 px-8 border-2 border-solid rounded"
+        @click="goToLogin"
       >
         Sign In
       </button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToLogin() {
+      this.$router.push("/login");
+    },
+    goToSignUp() {
+      this.$router.push("/signup");
+    },
+    goToHome() {
+      if (this.$store.isConnected){
+        this.$router.push("/logged");
+      } else {
+        this.$router.push("/");
+      }
+    },
+  },
+}
+</script>
--- frontend/src/components/LoggedTopBar.vue
+++ frontend/src/components/LoggedTopBar.vue
@@ -18,7 +18,25 @@
       >
       <a class="text-base text-gray-500">Explore</a>
     </div>
-    <ProfilePicture class="cursor-pointer" :path="`/${this.$store.user.username}`" :name="$store.user.name" />
+    <ProfilePicture v-if="$store.isConnected"
+    class="cursor-pointer"
+    :path="`/${this.$store.user.username}`" 
+    :name="$store.user.name" />
+    <div v-if="!$store.isConnected" class="flex items-center">
+      <a class="text-base text-gray-500">Explore</a>
+      <button
+        class="bg-pink-600 hover:bg-pink-700 text-white text-sm font-bold py-2 px-8 rounded mx-4"
+        @click="goToSignUp"
+      >
+        Sign Up
+      </button>
+      <button
+        class="border-pink-600 hover:border-pink-700 hover:text-pink-700 text-pink-600 text-sm font-bold py-2 px-8 border-2 border-solid rounded"
+        @click="goToLogin"
+      >
+        Sign In
+      </button>
+    </div>
   </div>
 </template>
 <script>
@@ -40,6 +58,12 @@ export default {
       this.$router.push({
         path: "/tasks",
       });
+    },
+    goToLogin() {
+      this.$router.push("/login");
+    },
+    goToSignUp() {
+      this.$router.push("/signup");
     },
   },
 };
